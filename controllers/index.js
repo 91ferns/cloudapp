8974bcb9066ae776225a6a79172ce9f16da7f12c
Stephen Parente <stephen@ingenuitydesign.com>
2014-06-20 14:12:52 -0400

    Initial commit of kraken application

--- controllers/index.js
+++ controllers/index.js
@@ -1,22 +1,18 @@
 'use strict';
 
 
-var IndexModel = require('../models/index'),
-    conf = require('nconf');
+var IndexModel = require('../models/index');
 
 
 module.exports = function (router) {
 
-    var snippet = process.env.snippet || false;
+    var model = new IndexModel();
 
-    console.log(snippet);
 
     router.get('/', function (req, res) {
-
-        res.render('index', {
-          browsersync: snippet
-        });
-
+        
+        res.render('index', model);
+        
     });
 
 };
