3226ed66d63e215b9c23f67531951e778b4d92c1
Stephen Parente <webmakersteve@gmail.com>
2014-07-01 23:04:37 -0400

    Ignored unbuilt files and got browsersync working again

--- gulpfile.js
+++ gulpfile.js
@@ -36,71 +36,72 @@ var node;
 
 // Lint JavaScript
 gulp.task('jshint', function () {
-  return gulp.src('app/scripts/**/*.js')
-    .pipe($.jshint())
-    .pipe($.jshint.reporter('jshint-stylish'))
-    .pipe($.jshint.reporter('fail'))
-    .pipe(reload({stream: true, once: true}));
+    return gulp.src('app/scripts/**/*.js')
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish'))
+        .pipe($.jshint.reporter('fail'))
+        .pipe(reload({stream: true, once: true}));
 });
 
 // Optimize Images
 gulp.task('images', function () {
-  return gulp.src('predist/images/**/*')
-    .pipe($.cache($.imagemin({
-      progressive: true,
-      interlaced: true
-    })))
-    .pipe(gulp.dest('public/images'))
-    .pipe(reload({stream: true, once: true}))
-    .pipe($.size({title: 'images'}));
+    return gulp.src('predist/images/**/*')
+        .pipe($.cache($.imagemin({
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest('public/images'))
+        .pipe(reload({stream: true, once: true}))
+        .pipe($.size({title: 'images'}));
 });
 
 // Automatically Prefix CSS
 gulp.task('styles:css', function () {
-  return gulp.src('predist/styles/**/*.css')
-    .pipe(uglify())
-    .pipe($.autoprefixer('last 1 version'))
-    .pipe(gulp.dest('public/styles'))
-    .pipe(reload({stream: true}))
-    .pipe($.size({title: 'styles:css'}));
+    return gulp.src('predist/styles/**/*.css')
+        //.pipe(uglify())
+        .pipe($.autoprefixer('last 1 version'))
+        .pipe(gulp.dest('public/css'))
+        .pipe(reload({stream: true}))
+        .pipe($.size({title: 'styles:css'}));
 });
 
 // Compile Sass For Style Guide Components (predist/styles/components)
 gulp.task('styles:components', function () {
-  return gulp.src('predist/styles/components/components.scss')
-    .pipe($.rubySass({
-      style: 'expanded',
-      precision: 10,
-      loadPath: ['predist/styles/components']
-    }))
-    .pipe($.autoprefixer('last 1 version'))
-    .pipe(gulp.dest('.tmp/styles/components'))
-    .pipe($.size({title: 'styles:components'}));
+    return gulp.src('predist/styles/components/components.scss')
+        .pipe($.rubySass({
+            style: 'expanded',
+            precision: 10,
+            loadPath: ['predist/styles/components']
+        }))
+        .pipe($.autoprefixer('last 1 version'))
+        .pipe(gulp.dest('.tmp/styles/components'))
+        .pipe($.size({title: 'styles:components'}));
 });
 
 // Compile Any Other Sass Files You Added (predist/styles)
 gulp.task('styles:scss', function () {
-  return gulp.src(['predist/styles/**/*.scss', '!predist/styles/components/components.scss'])
-    .pipe($.rubySass({
-      style: 'expanded',
-      precision: 10,
-      loadPath: ['predist/styles']
-    }))
-    .pipe(uglify())
-    .pipe($.autoprefixer('last 1 version'))
-    .pipe(gulp.dest('.tmp/styles'))
-    .pipe($.size({title: 'styles:scss'}));
+    return gulp.src(['predist/styles/**/*.scss', '!predist/styles/components/components.scss'])
+        .pipe($.rubySass({
+            style: 'expanded',
+            precision: 10,
+            loadPath: ['predist/styles']
+        }))
+        .pipe(uglify())
+        .pipe($.autoprefixer('last 1 version'))
+        .pipe(gulp.dest('.tmp/styles'))
+        .pipe($.size({title: 'styles:scss'}));
 });
 
-gulp.task('styles:less', function() {
-   return gulp.src('predist/styles/**/*.less')
-       .pipe(less({
-           paths: ['predist/styles']
-       }))
-       .pipe(uglify())
-       .pipe($.autoprefixer('last 1 version'))
-       .pipe(gulp.dest('.tmp/styles'))
-       .pipe($.size({title: 'styles:less'}));
+gulp.task('styles:less', function () {
+
+    return gulp.src('predist/styles/main.less')
+
+        .pipe(less())
+        //.pipe(uglify())
+        .pipe($.autoprefixer('last 1 version'))
+        .pipe(gulp.dest('public/css'))
+        .pipe(reload({stream: true}))
+        .pipe($.size({title: 'styles:less'}));
 });
 
 // Output Final CSS Styles
@@ -108,40 +109,40 @@ gulp.task('styles', ['styles:components', 'styles:scss', 'styles:css']);
 
 // Scan Your HTML For Assets & Optimize Them
 gulp.task('html', function () {
-  return gulp.src('predist/**/*.html')
-    .pipe($.useref.assets({searchPath: '{.tmp,predist}'}))
-    // Concatenate And Minify JavaScript
-    .pipe($.if('*.js', $.uglify()))
-    // Concatenate And Minify Styles
-    .pipe($.if('*.css', $.csso()))
-    // Remove Any Unused CSS
-    // Note: If not using the Style Guide, you can delete it from
-    // the next line to only include styles your project uses.
-    .pipe($.if('*.css', $.uncss({ html: ['predist/index.html','predist/styleguide/index.html'] })))
-    .pipe($.useref.restore())
-    .pipe($.useref())
-    // Update Production Style Guide Paths
-    .pipe($.replace('components/components.css', 'components/main.min.css'))
-    // Minify Any HTML
-    .pipe($.minifyHtml())
-    // Output Files
-    .pipe(gulp.dest('public'))
-    .pipe($.size({title: 'html'}));
+    return gulp.src('predist/**/*.html')
+        .pipe($.useref.assets({searchPath: '{.tmp,predist}'}))
+        // Concatenate And Minify JavaScript
+        .pipe($.if('*.js', $.uglify()))
+        // Concatenate And Minify Styles
+        .pipe($.if('*.css', $.csso()))
+        // Remove Any Unused CSS
+        // Note: If not using the Style Guide, you can delete it from
+        // the next line to only include styles your project uses.
+        .pipe($.if('*.css', $.uncss({ html: ['predist/index.html', 'predist/styleguide/index.html'] })))
+        .pipe($.useref.restore())
+        .pipe($.useref())
+        // Update Production Style Guide Paths
+        .pipe($.replace('components/components.css', 'components/main.min.css'))
+        // Minify Any HTML
+        .pipe($.minifyHtml())
+        // Output Files
+        .pipe(gulp.dest('public'))
+        .pipe($.size({title: 'html'}));
 });
 
 // Clean Output Directory
 gulp.task('clean', function (cb) {
-  rimraf('public', rimraf.bind({}, '.tmp', cb));
+    rimraf('public', rimraf.bind({}, '.tmp', cb));
 });
 
-var start =  function() {
-    node = spawn( 'node', ['index.js'], {stdio: 'inherit'});
-    node.on('close', function(code) {
-        if (code==8) gulp.log('Error detected');
+var start = function () {
+    node = spawn('node', ['index.js'], {stdio: 'inherit'});
+    node.on('close', function (code) {
+        if (code == 8) gulp.log('Error detected');
     })
 };
 
-var restart = function() {
+var restart = function () {
     node.kill();
     start();
 };
@@ -149,39 +150,41 @@ var restart = function() {
 // Watch Files For Changes & Reload
 gulp.task('serve', function () {
 
-    start();
-  var bs = browserSync.init(null, {});
 
-    bs.events.on('init', function(api) {
+    var bs = browserSync.init(null, {});
 
+    bs.events.on('init', function (api) {
+        var snippet = api.options.snippet;
+        process.env.snippet = snippet || false;
+        start();
     });
 
     gulp.watch(['controllers/*.js', 'models/*.js'], restart);
-    gulp.watch(['public/templates/**/*.dust','public/templates/*.dust'], reload);
+    gulp.watch(['public/templates/**/*.dust', 'public/templates/*.dust'], reload);
 
-  gulp.watch(['predist/**/*.html'], reload);
-  gulp.watch(['predist/styles/**/*.{css,scss}'], ['styles']);
-  gulp.watch(['.tmp/styles/**/*.css'], reload);
-  gulp.watch(['predist/scripts/**/*.js'], ['jshint']);
-  gulp.watch(['predist/images/**/*'], ['images']);
+    gulp.watch(['predist/**/*.html'], reload);
+    gulp.watch(['predist/styles/**/*.{css,scss,less}'], ['styles']);
+    gulp.watch(['.tmp/styles/**/*.css'], reload);
+    gulp.watch(['predist/scripts/**/*.js'], ['jshint']);
+    gulp.watch(['predist/images/**/*'], ['images']);
 });
 
 // Build Production Files, the Default Task
 gulp.task('default', ['clean'], function (cb) {
-  runSequence('styles', ['jshint', 'html', 'images'], cb);
+    runSequence('styles', ['jshint', 'html', 'images'], cb);
 });
 
 // Run PageSpeed Insights
 // Update `url` below to the public URL for your site
 gulp.task('pagespeed', pagespeed.bind(null, {
-  // By default, we use the PageSpeed Insights
-  // free (no API key) tier. You can use a Google
-  // Developer API key if you have one. See
-  // http://goo.gl/RkN0vE for info key: 'YOUR_API_KEY'
-  url: 'https://example.com',
-  strategy: 'mobile'
+    // By default, we use the PageSpeed Insights
+    // free (no API key) tier. You can use a Google
+    // Developer API key if you have one. See
+    // http://goo.gl/RkN0vE for info key: 'YOUR_API_KEY'
+    url: 'https://example.com',
+    strategy: 'mobile'
 }));
 
-process.on('exit', function() {
-   if (node) node.kill();
+process.on('exit', function () {
+    if (node) node.kill();
 });
