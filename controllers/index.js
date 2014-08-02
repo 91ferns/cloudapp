f94b170772d479f65e50a98f6e3bba4d706012fc
Stephen Parente <webmakersteve@gmail.com>
2014-08-01 22:25:22 -0400

    Style changes and some template stuff

--- controllers/index.js
+++ controllers/index.js
@@ -8,10 +8,14 @@ module.exports = function (router) {
 
     var model = new IndexModel();
 
-
     router.get('/', function (req, res) {
-        res.render('index', {browsersync: process.env.snippet || false});
-        
+        res.render('index', {
+          browsersync: process.env.snippet || false,
+          services: model.services,
+          applications: model.applications,
+          test: 'hey'
+        });
+
     });
 
 };
--- models/index.js
+++ models/index.js
@@ -3,6 +3,31 @@
 
 module.exports = function IndexModel() {
     return {
-        name: 'index'
+        name: 'index',
+        services: [
+          {
+            domain: "91ferns.com",
+            description: "91ferns blog and website",
+            host: "Azure"
+          },
+          {
+            domain: "patsys.com",
+            description: "Patsy's restaurant blog, store and website",
+            host: "Azure"
+          }
+        ],
+        applications: [
+          {
+            site: "Patsy's",
+            apps: [
+            {
+              name: "Store Manager"
+            },
+            {
+              name: "Analytics"
+            }
+            ]
+          }
+        ]
     };
 };
